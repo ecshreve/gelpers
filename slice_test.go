7a0dc3ed5f1f97af2e8f3eeb514c0e8952505027
Eric Shreve <ecshreve13@gmail.com>
2020-12-23 22:11:32 -0800

    gelpers/slice: check value in slice of ints

---
+++ slice_test.go
@@ -0,0 +1,56 @@
+package gelpers_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ecshreve/gelpers"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInIntSlice(t *testing.T) {
+	testcases := []struct {
+		desc     string
+		inpSlice []int
+		inpVal   int
+		expected bool
+	}{
+		{
+			desc:     "value exists",
+			inpSlice: []int{1, 2, 3},
+			inpVal:   2,
+			expected: true,
+		},
+		{
+			desc:     "value doesn't exist",
+			inpSlice: []int{1, 2, 3},
+			inpVal:   999,
+			expected: false,
+		},
+		{
+			desc:     "empty slice",
+			inpSlice: []int{},
+			inpVal:   999,
+			expected: false,
+		},
+		{
+			desc:     "nil slice",
+			inpSlice: nil,
+			inpVal:   999,
+			expected: false,
+		},
+	}
+
+	for _, testcase := range testcases {
+		t.Run(testcase.desc, func(t *testing.T) {
+			actual := gelpers.InIntSlice(testcase.inpSlice, testcase.inpVal)
+			assert.Equal(t, testcase.expected, actual)
+		})
+	}
+}
+
+func ExampleInIntSlice() {
+	s := []int{1, 2, 3}
+	fmt.Println(gelpers.InIntSlice(s, 2))
+	// Output: true
+}
